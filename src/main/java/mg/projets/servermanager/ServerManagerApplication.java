d535a778e1175418b83a8cf77e69c476a8bb3d81
Ericko Manarivo <manarivoericko4@gmail.com>
2022-03-27 21:38:55 +0300

    Implementation all methodes

--- src/main/java/mg/projets/servermanager/ServerManagerApplication.java
+++ src/main/java/mg/projets/servermanager/ServerManagerApplication.java
@@ -1,13 +1,50 @@
 package mg.projets.servermanager;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import mg.projets.servermanager.enumeration.Status;
+import mg.projets.servermanager.models.Server;
+import mg.projets.servermanager.repositories.ServerRepository;
 
 @SpringBootApplication
 public class ServerManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerManagerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository) {
+		return args -> {
+			serverRepository.save(new Server(null, "192.168.88.25", "Ubuntu", "16GB", "Personal PC",
+					"http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+		};
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration
+				.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+						"Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
+						"Access-Control-Allow-Credentials", "Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 
 }
--- src/main/java/mg/projets/servermanager/controllers/ServerController.java
+++ src/main/java/mg/projets/servermanager/controllers/ServerController.java
@@ -1,15 +1,20 @@
 package mg.projets.servermanager.controllers;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,44 +32,74 @@ import mg.projets.servermanager.services.implementation.ServerServiceImplementat
 @RequestMapping("/server")
 @RequiredArgsConstructor
 public class ServerController {
-    private final ServerServiceImplementation serverService;
+        private final ServerServiceImplementation serverService;
 
-    @GetMapping("/list")
-    public ResponseEntity<Response> getServers() {
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(LocalDateTime.now())
-                        .data(Map.of("servers", serverService.list(20)))
-                        .message("Servers retrieved")
-                        .status(OK)
-                        .statusCode(OK.value())
-                        .build());
-    }
+        @GetMapping("/list")
+        public ResponseEntity<Response> getServers() throws InterruptedException {
+                TimeUnit.SECONDS.sleep(3);
+                return ResponseEntity.ok(
+                                Response.builder()
+                                                .timeStamp(LocalDateTime.now())
+                                                .data(Map.of("servers", serverService.list(20)))
+                                                .message("Servers retrieved")
+                                                .status(OK)
+                                                .statusCode(OK.value())
+                                                .build());
+        }
 
-    @GetMapping("/list/{ipAddress}")
-    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
-        Server server = serverService.ping(ipAddress);
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(LocalDateTime.now())
-                        .data(Map.of("server", server))
-                        .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
-                        .status(OK)
-                        .statusCode(OK.value())
-                        .build());
-    }
+        @GetMapping("/ping/{ipAddress}")
+        public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+                Server server = serverService.ping(ipAddress);
+                return ResponseEntity.ok(
+                                Response.builder()
+                                                .timeStamp(LocalDateTime.now())
+                                                .data(Map.of("server", server))
+                                                .message(server.getStatus() == Status.SERVER_UP ? "Ping success"
+                                                                : "Ping failed")
+                                                .status(OK)
+                                                .statusCode(OK.value())
+                                                .build());
+        }
 
-    @PostMapping("/save")
-    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(LocalDateTime.now())
-                        .data(Map.of("server", serverService.create(
-                                server)))
-                        .message("Server created")
-                        .status(CREATED)
-                        .statusCode(CREATED.value())
-                        .build());
-    }
+        @PostMapping("/save")
+        public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+                return ResponseEntity.ok(
+                                Response.builder()
+                                                .timeStamp(LocalDateTime.now())
+                                                .data(Map.of("server", serverService.create(
+                                                                server)))
+                                                .message("Server created")
+                                                .status(CREATED)
+                                                .statusCode(CREATED.value())
+                                                .build());
+        }
 
+        @GetMapping("/get/{id}")
+        public ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
+                return ResponseEntity.ok(
+                                Response.builder()
+                                                .timeStamp(LocalDateTime.now())
+                                                .data(Map.of("server", serverService.get(id)))
+                                                .message("Server retrieved")
+                                                .status(OK)
+                                                .statusCode(OK.value())
+                                                .build());
+        }
+
+        @DeleteMapping("/delete/{id}")
+        public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+                return ResponseEntity.ok(
+                                Response.builder()
+                                                .timeStamp(LocalDateTime.now())
+                                                .data(Map.of("deleted", serverService.delete(id)))
+                                                .message("Server deleted")
+                                                .status(OK)
+                                                .statusCode(OK.value())
+                                                .build());
+        }
+
+        @GetMapping(path = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
+        public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+                return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/Downloads/images/" + fileName));
+        }
 }
